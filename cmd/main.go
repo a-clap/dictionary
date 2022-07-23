e7a20e1ede62c4e8d685dae07985432745cfa314
a-clap <clapinskiadam@gmail.com>
2022-07-23 21:47:48 +0200

    Support for mymemory.
    Used stringer to generate language_string.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/a-clap/dictionary/internal/logger"
+	"github.com/a-clap/dictionary/internal/translate"
 	"github.com/a-clap/dictionary/internal/translate/mymemory"
-	"os"
 )
 
 func main() {
-	apikey, found := os.LookupEnv("MY_MEMORY_API_KEY")
-	if !found {
-		panic("MY_MEMORY_API_KEY not found in ENV")
-	}
-
-	d := mymemory.New(apikey)
-	fmt.Println(d)
+	l := logger.NewDevelopment()
+	mem := mymemory.New(l)
+	s := mem.Translate("hello", translate.English)
+	fmt.Println(s)
 }
--- internal/translate/mymemory/mymemory.go
+++ internal/translate/mymemory/mymemory.go
@@ -1,15 +1,63 @@
 package mymemory
 
-import "github.com/a-clap/dictionary/internal/translate"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/a-clap/dictionary/internal/logger"
+	"github.com/a-clap/dictionary/internal/translate"
+	"net/http"
+	"net/url"
+)
 
 type MyMemory struct {
-	apiKey string
+	logger.Logger
 }
 
-func New(apiKey string) *MyMemory {
-	return &MyMemory{apiKey: apiKey}
+type responseData struct {
+	Text  string `json:"translatedText"`
+	Match int    `json:"match"`
+}
+
+type response struct {
+	Data           responseData `json:"responseData"`
+	ResponseStatus int          `json:"responseStatus"`
+}
+
+func New(logger logger.Logger) *MyMemory {
+	return &MyMemory{logger}
+}
+
+func getQuery(word string, lang translate.Language) string {
+	const getUrl = "https://api.mymemory.translated.net/get?q="
+	word = url.QueryEscape(word)
+	langPair := "langpair="
+
+	if lang == translate.English {
+		langPair += "en|pl"
+	} else {
+		langPair += "pl|en"
+	}
+	return fmt.Sprintf("%s%s&%s", getUrl, word, langPair)
+
 }
 
 func (d *MyMemory) Translate(word string, lang translate.Language) (translation string) {
-	return ""
+	s := getQuery(word, lang)
+	d.Infof("query = %s, for word = %s, lang = %+v", s, word, lang)
+
+	resp, err := http.Get(s)
+	if err != nil {
+		d.Errorf("error %v", err)
+		return
+	}
+
+	var r response
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		d.Errorf("error decoding json %v", err)
+		return
+	}
+	d.Infof("decoded json %+v", r)
+
+	return r.Data.Text
 }

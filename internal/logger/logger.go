313e4280967f822cadccbcfc77838d9f7796489c
a-clap <clapinskiadam@gmail.com>
2022-07-24 11:09:22 +0200

    Add SetLevel for logger interface, change time layout. Go tidy.

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -5,12 +5,24 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+type Level int64
+
+const (
+	Info Level = iota
+	Warn
+	Debug
+	Error
+	Fatal
+	Panic
+)
+
 type Logger interface {
 	Errorf(format string, args ...interface{})
 	Fatalf(format string, args ...interface{})
 	Infof(format string, args ...interface{})
 	Warnf(format string, args ...interface{})
 	Debugf(format string, args ...interface{})
+	SetLevel(level Level)
 }
 
 type Dummy struct {
@@ -21,6 +33,7 @@ func (d Dummy) Fatalf(string, ...interface{}) {}
 func (d Dummy) Infof(string, ...interface{})  {}
 func (d Dummy) Warnf(string, ...interface{})  {}
 func (d Dummy) Debugf(string, ...interface{}) {}
+func (d Dummy) SetLevel(Level)                {}
 
 type Standard struct {
 	*zap.SugaredLogger
@@ -31,9 +44,38 @@ func NewDummy() Dummy {
 }
 
 func NewDevelopment() Standard {
-	logger, _ := zap.NewDevelopment(zap.IncreaseLevel(zapcore.InfoLevel))
+	//atom := zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	cfg := zap.NewDevelopmentConfig()
+	cfg.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.000")
 
+	logger, _ := cfg.Build()
 	return Standard{
 		SugaredLogger: logger.Sugar(),
 	}
+}
+
+func matchLevel(level Level) zapcore.Level {
+	switch level {
+	case Info:
+		return zapcore.InfoLevel
+	case Warn:
+		return zapcore.WarnLevel
+	case Debug:
+		return zapcore.DebugLevel
+	case Error:
+		return zapcore.ErrorLevel
+	case Fatal:
+		return zapcore.FatalLevel
+	case Panic:
+		return zapcore.PanicLevel
+	}
+
+	return zapcore.PanicLevel
+}
+
+func (s *Standard) SetLevel(level Level) {
+	lvl := matchLevel(level)
+	s.SugaredLogger = s.SugaredLogger.Desugar().
+		WithOptions(zap.IncreaseLevel(lvl)).
+		Sugar()
 }

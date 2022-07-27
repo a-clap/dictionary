afd53469cb2a9838ff40e0b6f7681e82bb0241fa
a-clap <clapinskiadam@gmail.com>
2022-07-27 20:17:54 +0200

    Testing internal packages

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,58 @@ package main
 import (
 	"fmt"
 	"github.com/a-clap/dictionary/internal/logger"
+	"github.com/a-clap/dictionary/internal/merriamw"
 	"github.com/a-clap/dictionary/internal/translate"
 	"github.com/a-clap/dictionary/internal/translate/mymemory"
+	"log"
+	"os"
 )
 
 func main() {
+	thKey, ok := os.LookupEnv("MW_TH_KEY")
+	if !ok {
+		log.Fatalln("MW_TH_KEY not defined")
+	}
+
 	l := logger.NewDevelopment()
-	mem := mymemory.New(l)
-	s := mem.Translate("hello", translate.English)
-	fmt.Println(s)
+	thesaurus := merriamw.NewThesaurus(thKey, &l)
+
+	t, err := thesaurus.Translate("good")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("def =", t.ShortDef())
+	fmt.Println("synonyms =", t.Synonyms())
+	fmt.Println("antonyms = ", t.Antonyms())
+
+	dictTest()
+
+	mymemtest()
+}
+
+func mymemtest() {
+	l := logger.NewDevelopment()
+	mem := mymemory.New(&l)
+	brain := mem.Translate("mózg", translate.Polish)
+	fmt.Println(brain)
+
+}
+
+func dictTest() {
+	dictKey, ok := os.LookupEnv("MW_DICT_KEY")
+	if !ok {
+		log.Fatalln("MW_DICT_KEY not defined in env")
+	}
+
+	l := logger.NewDevelopment()
+	dict := merriamw.NewDictionary(dictKey, &l)
+
+	brain := "brain"
+	data, err := dict.Translate(brain)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("shortdef for", brain, "are:\r\n", data.ShortDef())
+	fmt.Println(data.AudioFiles())
 }

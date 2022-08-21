cc2a6f344af65dc87b347da7bf29be1cbd978e02
a-clap <clapinskiadam@gmail.com>
2022-08-21 12:24:55 +0200

    Testing auth.

--- pkg/server/users_test.go
+++ pkg/server/users_test.go
@@ -6,6 +6,7 @@ package server_test
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/a-clap/dictionary/internal/auth"
 	"github.com/a-clap/dictionary/pkg/server"
@@ -307,4 +308,159 @@ func TestServer_loginUser(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestServer_authUser(t *testing.T) {
+	type fields struct {
+		h server.Handler
+	}
+	type in struct {
+		url    string
+		method string
+		body   string
+	}
+	type out struct {
+		code int
+		body string
+	}
+	type params struct {
+		name string
+		in   in
+		out  out
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		params []params
+	}{
+		{
+			name: "add user, login, incorrect auth",
+			fields: fields{
+				h: auth.NewMemoryStore([]byte("key"), 1*time.Minute),
+			},
+			params: []params{
+				{
+					name: "add user",
+					in: in{
+						url:    "/api/user/add",
+						method: http.MethodPost,
+						body:   `{"name": "adam", "password": "pwd"}`,
+					},
+					out: out{
+						code: http.StatusCreated,
+						body: `{"name":"adam"}`,
+					},
+				},
+				{
+					name: "login user",
+					in: in{
+						url:    "/api/user/login",
+						method: http.MethodPost,
+						body:   `{"name": "adam", "password": "pwd"}`,
+					},
+					out: out{
+						code: http.StatusOK,
+						body: "token",
+					},
+				},
+				{
+					name: "use auth - wrong body",
+					in: in{
+						url:    "/api/translate/ping",
+						method: http.MethodGet,
+						body:   `{"name": "adam", "password": "pwd"}`,
+					},
+					out: out{
+						code: http.StatusUnauthorized,
+						body: "token",
+					},
+				},
+				{
+					name: "use auth - wrong token",
+					in: in{
+						url:    "/api/translate/ping",
+						method: http.MethodGet,
+						body:   `{"token": "1234"}`,
+					},
+					out: out{
+						code: http.StatusUnauthorized,
+						body: "token",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := require.New(t)
+			s := server.New(tt.fields.h)
+
+			for i, param := range tt.params {
+
+				reader := bytes.NewBuffer([]byte(param.in.body))
+				request, err := http.NewRequest(param.in.method, param.in.url, reader)
+
+				req.Nil(err, "unexpected err on %v", i)
+
+				response := httptest.NewRecorder()
+				s.ServeHTTP(response, request)
+
+				assert.Equal(t, param.out.code, response.Code, "test: %s", param.name)
+				resp := response.Body.String()
+				assert.Contains(t, resp, param.out.body, "test: %s", param.name)
+			}
+		})
+	}
+
+}
+
+func TestServer_authUserToken(t *testing.T) {
+	type addUsers struct {
+		body string
+	}
+
+	users := []addUsers{{
+		body: `{"name": "adam", "password": "pwd"}`,
+	}}
+	// Prepare server
+	s := server.New(auth.NewMemoryStore([]byte("key"), time.Hour))
+
+	t.Run("add users", func(t *testing.T) {
+		for _, user := range users {
+			reader := bytes.NewBuffer([]byte(user.body))
+			request, err := http.NewRequest(http.MethodPost, "/api/user/add", reader)
+			require.Nil(t, err)
+
+			response := httptest.NewRecorder()
+			s.ServeHTTP(response, request)
+			require.Equal(t, http.StatusCreated, response.Code)
+		}
+	})
+
+	t.Run("authorize with correct token", func(t *testing.T) {
+		reader := bytes.NewBuffer([]byte(users[0].body))
+		request, err := http.NewRequest(http.MethodPost, "/api/user/login", reader)
+		require.Nil(t, err)
+
+		response := httptest.NewRecorder()
+		s.ServeHTTP(response, request)
+		require.Equal(t, http.StatusOK, response.Code)
+
+		resp := make(map[string]interface{})
+
+		err = json.NewDecoder(response.Body).Decode(&resp)
+		require.Nil(t, err)
+		v, ok := resp["token"]
+		require.True(t, ok)
+
+		// Use v as token
+		request, err = http.NewRequest(http.MethodGet, "/api/translate/ping", nil)
+		request.Header.Set("Authorization", v.(string))
+		require.Nil(t, err)
+		s.ServeHTTP(response, request)
+		require.Equal(t, http.StatusOK, response.Code)
+		expected := `{"message":"pong"}`
+		require.Equal(t, expected, response.Body.String())
+
+	})
 }
